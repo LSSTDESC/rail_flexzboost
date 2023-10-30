3feef5fbdbe10b9c9317e7b7c07c67aa3493413e
Drew Oldag <awoldag@uw.edu>
2023-10-30 15:48:07 -0700

    Removing this test because it doesn't actually test any code in this package.

--- tests/test_algos.py
+++ tests/test_algos.py
@@ -131,20 +131,3 @@ def test_missing_groupname_keyword():
                                              'reg:squarederror'}}
     stage = flexzboost.FlexZBoostEstimator.make_stage(**config_dict)
     assert stage.config_options['hdf5_groupname'] == "photometry"
-
-def test_wrong_modelfile_keyword():
-    RailStage.data_store.clear()
-    config_dict = {'zmin': 0.0, 'zmax': 3.0, 'nzbins': 301,
-                   'trainfrac': 0.75, 'bumpmin': 0.02,
-                   'bumpmax': 0.35, 'nbump': 3,
-                   'sharpmin': 0.7, 'sharpmax': 2.1,
-                   'nsharp': 3, 'max_basis': 35,
-                   'basis_system': 'cosine',
-                   'hdf5_groupname': 'photometry',
-                   'regression_params': {'max_depth': 8,
-                                             'objective':
-                                             'reg:squarederror'},
-                   'model': 'nonexist.pkl'}
-    with pytest.raises(FileNotFoundError):
-        pz_algo = flexzboost.FlexZBoostEstimator.make_stage(**config_dict)
-        assert pz_algo.model is None
