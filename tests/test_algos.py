b0cab6ab82c453dac0193e491d2ae481efa0d1c2
Tianqing Zhang <ztq1996@live.com>
2024-05-07 19:59:49 -0400

    change one_algo import path

--- tests/test_algos.py
+++ tests/test_algos.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 from rail.core.stage import RailStage
-from rail.utils.algo_utils import one_algo
+from rail.utils.testing_utils import one_algo
 from rail.utils.path_utils import RAILDIR
 from rail.estimation.algos import flexzboost
 import scipy.special
